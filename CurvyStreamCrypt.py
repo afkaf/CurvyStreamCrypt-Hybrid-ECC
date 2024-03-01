92dd969cadfbed8ae4177521d8fc6403a4d029bf
afkaf <62219143+afkaf@users.noreply.github.com>
2024-03-01 20:05:11 +0300

    Fix naming convention

--- CurvyStreamCrypt.py
+++ CurvyStreamCrypt.py
@@ -6,14 +6,15 @@ import secrets
 import blake3
 
 curve = registry.get_curve('brainpoolP256r1')
-CONTEXT = 'StreamCrypt-2db12ea77123cf47d288753b12914f61102d92a8b3a2d220c65e80a99004f3a7-by-afkaf'
+context = 'StreamCrypt-2db12ea77123cf47d288753b12914f61102d92a8b3a2d220c65e80a99004f3a7-by-afkaf'
+
 
 # STREAMCRYPT ENCRYPTION STUFF
 # Stream Cipher Encryption using BLAKE3 for key derivation and encryption. Salts are used in place of nonce.
 def encrypt_sc(data, keyword):
     salt = secrets.token_bytes(64)  # salt generation
-    initial_key = blake3.blake3(keyword + salt, derive_key_context=CONTEXT).digest(length=64)  # Hash password and salt
-    keystream = blake3.blake3(initial_key, derive_key_context=CONTEXT).digest(length=len(data))
+    initial_key = blake3.blake3(keyword + salt, derive_key_context=context).digest(length=64)  # Hash password and salt
+    keystream = blake3.blake3(initial_key, derive_key_context=context).digest(length=len(data))
     data_array = np.frombuffer(data, dtype=np.uint8)
     keystream_array = np.frombuffer(keystream, dtype=np.uint8)
     secret = np.bitwise_xor(data_array, keystream_array).tobytes()
@@ -21,35 +22,37 @@ def encrypt_sc(data, keyword):
 
 def decrypt_sc(secret, keyword):
     salt, encrypted_data = secret[:64], secret[64:]  # Extract salt and encrypted data
-    initial_key = blake3.blake3(keyword + salt, derive_key_context=CONTEXT).digest(length=64)  # Recreate initial key
-    keystream = blake3.blake3(initial_key, derive_key_context=CONTEXT).digest(length=len(encrypted_data))
+    initial_key = blake3.blake3(keyword + salt, derive_key_context=context).digest(length=64)  # Recreate initial key
+    keystream = blake3.blake3(initial_key, derive_key_context=context).digest(length=len(encrypted_data))
     encrypted_array = np.frombuffer(encrypted_data, dtype=np.uint8)
     keystream_array = np.frombuffer(keystream, dtype=np.uint8)
     data = np.bitwise_xor(encrypted_array, keystream_array).tobytes()
     return data
 
+
 # ECC ENCRYPTION STUFF
 # Elliptic Curve Cryptography (ECC) layer for asymmetric encryption. Uses ECC for key exchange and BLAKE3 for deriving a shared key.
-def encrypt_ECC(msg, pubKey):
-    ciphertextPrivKey = secrets.randbelow(curve.field.n)
-    sharedECCKey = ciphertextPrivKey * pubKey
-    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+def encrypt_ecc(msg, public_key):
+    ciphertext_private_key = secrets.randbelow(curve.field.n)
+    shared_ecc_key = ciphertext_private_key * public_key
+    secretKey = ecc_point_to_256_bit_key(shared_ecc_key)
     ciphertext = encrypt_sc(msg, secretKey)
-    ciphertextPubKey = ciphertextPrivKey * curve.g
-    return (ciphertext, ciphertextPubKey)
+    ciphertext_public_key = ciphertext_private_key * curve.g
+    return (ciphertext, ciphertext_public_key)
 
-def decrypt_ECC(encryptedMsg, privKey):
-    (ciphertext, ciphertextPubKey) = encryptedMsg
-    sharedECCKey = privKey * ciphertextPubKey
-    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+def decrypt_ecc(encrypted_msg, private_key):
+    (ciphertext, ciphertext_public_key) = encrypted_msg
+    shared_ecc_key = private_key * ciphertext_public_key
+    secretKey = ecc_point_to_256_bit_key(shared_ecc_key)
     plaintext = decrypt_sc(ciphertext, secretKey)
     return plaintext
+
 
 # POINT COMPRESSION AND KEY DERIVATION
 # Functions for converting ECC points into a format usable for encryption keys and compressing/decompressing ECC public keys for storage.
 def ecc_point_to_256_bit_key(point):
     point_bytes = int.to_bytes(point.x, 32, 'big') + int.to_bytes(point.y, 32, 'big')
-    return blake3.blake3(point_bytes, derive_key_context=CONTEXT).digest()
+    return blake3.blake3(point_bytes, derive_key_context=context).digest()
 
 def compress_point(point):
     return hex(point.x) + hex(point.y % 2)[2:]
@@ -64,19 +67,21 @@ def decompress_to_point(curve, compressed_key):
         return Point(curve, x, y)
     return Point(curve,x, p - y)
 
-# MAIN ENCRYPT/DECRYPT
-# High-level encryption/decryption and key creation interface.
-def create_pivate_key():
+
+#  MAIN ENCRYPT/DECRYPT
+#  High-level encryption/decryption and key creation interface.
+def create_private_key():
     return secrets.randbelow(curve.field.n)
 
-def create_public_key(privKey):
-    return privKey * curve.g
+def create_public_key(private_key):
+    return private_key * curve.g
 
-def decrypt(encryptedMsg, privKey):
-    encryptedMsg = encryptedMsg.split(',')
-    encryptedMsg = [bytes.fromhex(encryptedMsg[0])] + [decompress_to_point(curve,encryptedMsg[1])]
-    return decrypt_ECC(encryptedMsg, privKey)
+def decrypt(encrypted_msg, private_key):
+    encrypted_msg = encrypted_msg.split(',')
+    encrypted_msg = [bytes.fromhex(encrypted_msg[0])] + [decompress_to_point(curve,encrypted_msg[1])]
+    return decrypt_ecc(encrypted_msg, private_key)
 
+<<<<<<< Updated upstream:CurvyStreamCrypt.py
 def encrypt(msg, pubKey):
     encryptedMsg = encrypt_ECC(msg, pubKey)
     encryptedMsg = [encryptedMsg[0].hex()] + [compress_point(encryptedMsg[1])]
@@ -89,3 +94,9 @@ pubKey = create_public_key(privKey)
 emsg = encrypt(b'testingggggggg', pubKey)
 
 dmsg = decrypt(emsg, privKey)
+=======
+def encrypt(msg, public_key):
+    encrypted_msg = encrypt_ecc(msg, public_key)
+    encrypted_msg = [encrypted_msg[0].hex()] + [compress_point(encrypted_msg[1])]
+    return ','.join(encrypted_msg)
+>>>>>>> Stashed changes:curvystreamcrypt.py
